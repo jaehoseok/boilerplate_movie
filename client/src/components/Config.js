acaf44d473fe618df455905ef7d1b1cd226c9711
jaehoseok <woghok@inu.ac.kr>
2021-03-04 18:42:26 +0900

    first

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = '45d7b5419c42eebe99a0e41ecaa91c5d';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
