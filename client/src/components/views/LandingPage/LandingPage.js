9bfb0255b356054b6fcce0378cbe728d6e42fc55
jaehoseok <woghok@inu.ac.kr>
2021-03-05 14:09:58 +0900

    영화 정보 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,21 +9,34 @@ function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
+        fecthMovies(endpoint)
+
+    }, [])
+
+    const fecthMovies = (endpoint) => {
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
 
             console.log(response)
 
-            setMovies([response.results])
+            setMovies([... Movies, ...response.results])
             setMainMovieImage(response.results[0])
+            setCurrentPage(response.page)
         })
-    }, [])
+    };
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage+1}`;
+
+        fecthMovies(endpoint);
+    };
 
     return (
         <div style={{ width: '100%', margin: '0'}}>
@@ -45,10 +58,10 @@ function LandingPage() {
                 {/* Movie Grid Cards */}
                 <Row gutter={[16, 16]}>
                     {Movies && Movies.map((movie, index) => (
+
                         <React.Fragment key={index}>
-                            <GridCards 
-                                image={movie.poster_path ? 
-                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                            <GridCards
+                                image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                                 movieId={movie.id}
                                 movieName={movie.original_title}
                             />
@@ -59,7 +72,7 @@ function LandingPage() {
 
             </div>
             <div style={{ display: 'flex', justifyContent:'center'}}>
-                <button> Load More </button>
+                <button onClick={loadMoreItems}> Load More </button>
             </div>
         </div>
     )
